4a0a1172d7170c098819421d78d790db715d0f55
Alicia Weenum <weenum.alicia@gmail.com>
2020-10-24 15:05:53 -0400

    commit tree 1d6f184964adaf9e4db8cc8c00908c6bb6c5f625

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+
+def prime?(integer)
+        if integer <= 1 || integer == 0
+                return false 
+        elsif integer <= 3 
+                return true
+        else 
+        (2..integer-1).none? do |n| integer % n == 0 
+                #list of numbers based on how many interger there is .to_a is to an array
+                #how do we do integer value of n that exist in the above array
+                #return true #for prime number. define an integer as prime.
+        end 
+        end
+
+  #return false # for non-prime numbers
+end
+
+# check if it's divisible by itself and also greater than 1
